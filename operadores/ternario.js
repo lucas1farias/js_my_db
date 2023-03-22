546743ec25e53d35550dfeea8de2e34e982b87f2
Lucas-far <mizoguchia@hotmail.com>
2023-03-22 10:34:04 -0300

    first commit

---
+++ operadores/ternario.js
@@ -0,0 +1,30 @@
+
+
+// Versão JS do list comprehension Python
+// Encurtador de expressões
+
+// Os consoles abaixo são o equivalente em Python: ['sim' if 34 % 17 == 0 else 'não']
+// (34 % 17 === 0 ? 'sim' : 'não') = (if ? retorno if : retorno else)
+console.log(`34 é divisível por 17? ${(34 % 17 === 0 ? 'sim' : 'não')}`)
+console.log(`34 é divisível por 3? ${34 % 3 === 0 ? 'sim' : 'não'}`)
+
+// function isAdult({age}) {
+//   /*
+//   Forma menos avançada
+//   if (age >= 18) {
+//     return 'Adulto'
+//   } else {
+//     return 'Menor de idade'
+//   }
+//   */
+
+//   // Forma avançada usando o operador ternário ?
+//   return age >= 18 ? 'Adulto' : 'Menor de idade'
+// }
+
+// console.log(isAdult({age: 17}))
+// console.log(isAdult({age: 18}))
+
+let isAdult = age => age >= 18 ? 'Adulto' : 'Menor de idade'
+console.log('[DECLARATIVA]', isAdult(18))
+console.log('[DECLARATIVA]', isAdult(17))
